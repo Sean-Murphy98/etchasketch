aa903b6ec4a3f2def3ae5a3dfb6e8fc043ddc00d
Sean-Murphy98 <92535432+Sean-Murphy98@users.noreply.github.com>
2025-09-05 05:23:41 -0700

    basic etchasketch implementation

--- source.js
+++ source.js
@@ -1,3 +1,6 @@
+function onHover(square){
+    square.style.background = "black";
+}
 const container = document.querySelector("#container")
 for (let j=0; j<16; j++){
     const row = document.createElement("divRow");
@@ -8,6 +11,9 @@ for (let j=0; j<16; j++){
     for (let i=0; i<16; i++){
         const square = document.createElement("div");
         square.classList.add("square");
+        square.addEventListener("mouseenter", () => {
+            onHover(square);
+        })
         row.appendChild(square);
     }
 }
