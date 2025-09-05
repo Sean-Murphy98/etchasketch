ff995f503ed6507218be647d72b3dbf1ff032dd7
Sean-Murphy98 <92535432+Sean-Murphy98@users.noreply.github.com>
2025-09-05 06:30:46 -0700

    Base etchasketch functionality with grid changing options

--- source.js
+++ source.js
@@ -1,19 +1,41 @@
 function onHover(square){
     square.style.background = "black";
 }
-const container = document.querySelector("#container")
-for (let j=0; j<16; j++){
-    const row = document.createElement("divRow");
-    row.classList.add("row");
+function gridCreation(size){
+    const container = document.querySelector("#container")
+    for (let j=0; j<size; j++){
+        const row = document.createElement("divRow");
+        row.classList.add("row");
 
-    container.appendChild(row);
+        container.appendChild(row);
 
-    for (let i=0; i<16; i++){
-        const square = document.createElement("div");
-        square.classList.add("square");
-        square.addEventListener("mouseenter", () => {
-            onHover(square);
-        })
-        row.appendChild(square);
+        for (let i=0; i<size; i++){
+            const square = document.createElement("div");
+            square.classList.add("square");
+            square.addEventListener("mouseenter", () => {
+                onHover(square);
+            })
+            row.appendChild(square);
+        }
     }
 }
+
+function gridDeletion(){
+    const container = document.querySelector("#container");
+
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+    }
+}
+gridCreation(16)
+button = document.querySelector("#button")
+button.addEventListener("click", () =>{
+    let userSize = prompt("Choose a new NxN grid size (max 100):");
+    if (userSize>0 && userSize<=100){
+        gridDeletion()
+        gridCreation(userSize)
+    }
+    else{
+        alert("Please choose a size between 0 and 100")
+    }
+})
